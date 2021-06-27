e688f4967bde59cc5ae33b7d43a9363555a0525e
SysAsriel <ea8303pax12@outlook.com>
2021-06-27 17:40:13 +0800

    test.py
    It can to try!

--- test.py
+++ test.py
@@ -2,4 +2,13 @@
 
 n = int(input())
 seed = 0
-#Code Time
+# Code Time
+while n<=1:
+    if (n%2 == 0): # 如果n是偶数
+        n /= 2
+    else:          # 如果n是奇数
+        n = (3*n+1)/2 
+    
+    seed += 1
+
+print(seed)
